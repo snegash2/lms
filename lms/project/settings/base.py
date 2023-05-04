ae7846d4bc143cad23f1592f48846ed7f0d2cb6a
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-04 11:03:03 +0300

    tailwind css

--- lms/project/settings/base.py
+++ lms/project/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',  # new
 ]
 
 MIDDLEWARE = [
@@ -35,7 +36,7 @@ ROOT_URLCONF = 'lms.project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/tinsae/Desktop/projects/LMS/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
