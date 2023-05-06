df249979659d25433a9a7aa015495e646f2a7763
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-06 18:05:35 +0300

    adding  Text Video Image and File models to relate with course model

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -46,3 +49,51 @@ class Module(models.Model):
         return f'{self.order}. {self.title}'
 
 
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE,limit_choices_to={'model__in':(
+                                'text',
+                                'video',
+                                'image',
+                                'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+    class Meta:
+        ordering = ['order']
+
+
+
+class GenericItem(models.Model):
+    teacher = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(GenericItem):
+    content = models.TextField()
+
+
+class File(GenericItem):
+    file = models.FileField(upload_to='files')
+
+
+class Image(GenericItem):
+       file = models.FileField(upload_to='images')
+
+
+class Video(GenericItem):
+    url = models.URLField()
