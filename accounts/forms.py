613c9e3433b4d998c080858e409877c94994ba14
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-11 20:50:47 +0300

    adding permissions for instructors group

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,9 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-
-    username = forms.CharField(label='Username', max_length=150,widget=forms.TextInput(attrs={"class":"bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"}))
+    username = forms.CharField(
+        label='Username',
+        max_length=150,
+        widget=forms.TextInput(attrs={"class":"bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"}))
     password = forms.CharField(label='Password')
 
