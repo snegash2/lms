2e8ac5fff66fbd40dd66d55972d4136036687975
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-12 12:28:20 +0300

    updating list and form templates

--- courses/views.py
+++ courses/views.py
@@ -3,20 +3,12 @@ from django.views.generic.edit import CreateView,UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.urls import reverse_lazy
 from .models import Course
-# class ManageCourseListView(ListView):
-#     model = Course
-#     template_name = 'courses/manage/course/list.html'
-#     def get_queryset(self):
-#         qs = super().get_queryset()
-#         return qs.filter(teacher=self.request.user)
-
-
 
 # common behavior for all classes this class return courses which create only by currently logedin user
 class OwnerMixin:
     def get_queryset(self):
         qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
+        return qs.filter(teacher=self.request.user)
 
 
 # assign currently logedin user during editing of course
