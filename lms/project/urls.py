1d767bb22e78041ca7fff26e1319e1e20ad3886e
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-12 12:28:20 +0300

    updating list and form templates

--- lms/project/urls.py
+++ lms/project/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
     path('', index, name='index')
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
