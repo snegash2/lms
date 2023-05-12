603f4b1ab5a8d720a2d955c49889beacc3b395ce
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-12 15:48:11 +0300

    pushing for first time in prepod branch

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 #
-# app_name = "courses"
+app_name = "courses"
 urlpatterns = [
         path('mine/',views.ManageCourseListView.as_view(),name='manage_course_list'),
         path('create/',views.CourseCreateView.as_view(),name='course_create'),
