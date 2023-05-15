eee18572939ff61bc4a4e607e181eabe96ef1cae
codewiztinsing <tinsingjobs2k@gmail.com>
2023-05-15 11:16:53 +0300

    adding views to manage course modules

--- courses/views.py
+++ courses/views.py
@@ -47,7 +47,29 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
 
-
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+        data=data)
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+        id=pk,
+        owner=request.user)
+        return super().dispatch(request, pk)
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+        'course': self.course,'formset': formset})
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+        'course': self.course,
+        'formset': formset})
 class ContentCreateUpdateView(TemplateResponseMixin, View):
     module = None
     model = None
