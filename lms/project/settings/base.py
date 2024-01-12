2f73fbf3e5b1a5ce3e6dcb76255dd45c9d6bfcc9
codewiztinsing <tinsingjobs2k@gmail.com>
2024-01-12 16:28:18 +0300

    uploading images

--- lms/project/settings/base.py
+++ lms/project/settings/base.py
@@ -151,8 +151,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')# type:ignore
+
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')# type:ignore
+
 STATICFILES_DIRS = [
   BASE_DIR / 'static' / 'css' / 'landing',
   BASE_DIR /'static' / 'css' / 'courses',
